602dca8a1a67e842fa317851cee41d53d77d5cbc
Collins Rop <145935789+collinsrop@users.noreply.github.com>
2024-08-15 01:21:21 +0300

    Add files via upload
    INSTALLING APPLICATIONS
    Install node to your pc => (sudo apt install nodejs npm)
    Check the presence  of Node.js => (node --v)
    Install typescript globally => (npm i typescript --g)
    WRITING TS CODE:
    Using VS code (code editor), run the terminal window
    Install typecriptconfig.js file and edit the source files folder and the out directory (outDir./) 
    Create a simple file with an extension .ts i.e (index.ts) and add some vanilla JavaScript.
    In the terminal window run tsc and click enter
    Check the dist folder, if a ner javascript folder has been added to it then there is a successfull compilation

---
+++ index.ts
@@ -0,0 +1,13 @@
+let userInfo = [
+	{name: "John", age:24, complexion:"dark"},
+	{name: "Anne", age:23, complexion:"light"},
+	{name: "Daniel", age:14, complexion:"white"},
+];
+console.log(userInfo.map(u=>u.name));
+let weeks = ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'];
+
+let currentDay = new Date().getDay();
+let dayOfTheWeek = weeks[currentDay];
+console.log(dayOfTheWeek);
+console.log(new Date().getSeconds())
+
